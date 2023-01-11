fd60f7ec0be7cd1d97e1aedebea93245fda74c19
Shuss1988 <iron-romanjuarezjesus@gmail.com>
2023-01-11 18:13:39 +0100

    creacion de carpeta

---
+++ 1/06-ejercicio.js
@@ -0,0 +1,19 @@
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+  }
+
+for (let i = 0; i <= 9; i++) {
+    if(i % 2 === 0) {
+    console.log(i);
+    }
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    if(i === 10 ) {
+    console.log("dormido");
+    }
+    else {
+    console.log("intentando dormir 🐑");
+    }
+
+  }
