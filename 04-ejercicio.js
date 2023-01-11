f310e31b3349c90a454e2253c41d36bd256a89d9
Shuss1988 <iron-romanjuarezjesus@gmail.com>
2023-01-11 08:47:20 +0100

    first commit

---
+++ 04-ejercicio.js
@@ -0,0 +1,14 @@
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+//console.log(avengers[1]);
+//console.log(avengers[2]);
+
+avengers[0] = "IRONMAN";
+console.log(avengers);
+
+console.log(avengers.length);
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push( "Morty","Summer");
+console.log(rickAndMortyCharacters);
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
